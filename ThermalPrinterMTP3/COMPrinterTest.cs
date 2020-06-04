30ae40c50963868abb24786a908d76bab93bd30a
Jorge Lucas <lukasrochaaraujo@gmail.com>
2020-06-03 22:22:53 -0300

    Add COMPrinterTest.

---
+++ ThermalPrinterMTP3/COMPrinterTest.cs
@@ -0,0 +1,17 @@
+﻿namespace ThermalPrinterMTP3
+{
+    public static class COMPrinterTest
+    {
+        public static void Print()
+        {
+            var mtp3 = new COMPrinter("COM9");
+            mtp3.AppendRowToBuffer("\r\n");
+            mtp3.AppendRowToBuffer("THERMAL RECEIPT PRINTER");
+            mtp3.AppendRowToBuffer("- Model: MTP-3");
+            mtp3.AppendRowToBuffer("- Print Width: 80mm");
+            mtp3.AppendRowToBuffer("- Print Speed: 90mm/s");
+            mtp3.AppendRowToBuffer("\r\n\r\n\r\n");
+            mtp3.SendBufferToPrinter();
+        }
+    }
+}
